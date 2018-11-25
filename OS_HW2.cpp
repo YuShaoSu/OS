5b9b60f8b04f7b09c9864f9d52730a29f84db672
YuShaoSu <n270882@gmail.com>
2018-11-25 21:42:25 +0800

    上傳到workstation寫

--- OS_HW2.cpp
+++ OS_HW2.cpp
@@ -17,7 +17,7 @@ BmpImage shear_vertical(BmpImage pic, int nw, int nh);
 BmpImage shear_horizontal(BmpImage pic, int nw, int nh);
 
 // TODO: add your pthread codes to speed up the program
-
+pthread_t p[15];
 int main(){
 	
 	struct timeval  tv1, tv2;
@@ -63,6 +63,8 @@ int main(){
 	printf("0516016%f seconds\n", (double)(tv2.tv_usec - tv1.tv_usec) / 1000000 + (double)(tv2.tv_sec - tv1.tv_sec)); 
 	
 	// TODO: add your pthread codes to speed up the program
+	pid_t p[16];
+	pthread_create(p[0], NULL, bilinear, para);
 	
 	return 0;
 } 
